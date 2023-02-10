bbf01d81b2efab1f0dcbda8a256d0afb2c4df4c3
Sandeep-morya <laxmidassmorya@gmail.com>
2023-02-10 15:52:37 +0530

    done for today will work on sunday

---
+++ db/mongo/index.js
@@ -0,0 +1,10 @@
+﻿const { connect, set } = require("mongoose");
+
+set("strictQuery", true);
+require("dotenv").config();
+const { URL, DBNAME, QUERY, PORT } = process.env;
+
+module.exports = {
+	PORT,
+	connection: connect(URL + DBNAME + QUERY),
+};
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-﻿
+﻿const express = require("express");
+const { PORT, connection } = require("./db/mongo");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, async () => {
+	console.log("Server is running on PORT: " + PORT);
+	try {
+		await connection;
+	} catch {
+		console.log(" But Unable to Connect With Database");
+	}
+});
